e738ed9e60d7cb40fc235b163df204fd08051db8
leighwoltman <ridgequail@gmail.com>
2015-07-22 19:45:30 -0300

    Adding website side, database query not complete.

---
+++ GaugeWebsite/server.js
@@ -0,0 +1,43 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require('body-parser'),
+    errorHandler = require('errorhandler'),
+    methodOverride = require('method-override');
+
+var MongoJs = require('mongojs');
+
+var _Db = MongoJs.connect("mongodb://localhost:27017/gauge_db");
+
+var hostname = 'localhost';
+var port = 4567;
+var publicDir = __dirname + '/public';
+
+app.get("/", function (req, res) {
+  res.redirect("/index.html");
+});
+
+app.use(methodOverride());
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static(publicDir));
+
+app.get('data/data.json', function (req, res) {
+  //_Db.collection('data').find({ level: { $exists: true } }, { _id: true, level: true }).sort({ _id: 1 }).limit(2000, function () {
+
+    //res.send(json);
+  //});
+});
+
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
+
+console.log("Simple static server showing %s listening at http://%s:%s", publicDir, hostname, port);
+
+app.listen(port, hostname);
